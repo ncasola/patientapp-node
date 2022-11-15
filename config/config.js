c7bff8013b5963d413d8d6e79421dbdb06a49f41
Néstor Casola Jiménez <ncasolajimenez@gmail.com>
2022-11-15 15:53:30 +0000

    Fix database

--- config/config.js
+++ config/config.js
@@ -1,17 +1,12 @@
 "use strict";
+
 module.exports = {
   development: {
     dialect: "sqlite",
     storage: "./database.sqlite",
   },
   production: {
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    host: process.env.RDS_HOSTNAME,
-    dialect: "mariadb",
-    dialectOptions: {
-      ssl: true,
-    },
+    dialect: "sqlite",
+    storage: "./database.sqlite",
   },
 };
